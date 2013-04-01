a8878195f2add8b9515a1106405d9e3848074de8
garvankeeley <garvankeeley@gmail.com>
2013-04-01 18:32:02 +0000

    initial checkin

---
+++ InstallNameToolGui/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  InstallNameToolGui
+//
+//  Created by avenza on 2013-03-27.
+//  Copyright (c) 2013 avenza. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+-(void)openNewWindow:(NSString*)path;
+@end
